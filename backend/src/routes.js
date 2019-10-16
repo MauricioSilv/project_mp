3a38f2cf3f26e74018072632e2b40401e7e01cf0
MauricioSilv <mausilva828@gmail.com>
2019-10-16 09:48:29 -0300

    Users registration

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+
 const routes = new Router();
 
-routes.get('/', async (req, res) => {});
+routes.post('/users', UserController.store);
 
 export default routes;
