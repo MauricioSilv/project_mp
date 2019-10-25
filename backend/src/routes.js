58477befea8a4090f607656e1682064ae75edae9
mauriciosilv <mausilva828@gmail.com>
2019-10-25 09:31:53 -0300

    config multer for multiform-data

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
-
+import multer from 'multer';
+import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import EquipTypeController from './app/controllers/EquipTypeController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const uploads = multer(multerConfig);
 
 routes.post('/sessions', SessionController.store);
 
@@ -18,5 +20,8 @@ routes.get('/types', EquipTypeController.index);
 routes.post('/types', EquipTypeController.store);
 routes.put('/types/:idType', EquipTypeController.update);
 routes.delete('/types/:idType', EquipTypeController.destroy);
+routes.post('/files', uploads.single('file'), (req, res) => {
+  return res.json({ ok: true });
+});
 
 export default routes;
