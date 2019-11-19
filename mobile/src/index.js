74a599c804e7105978b580bb3c0e7db85c4d92af
mauriciosilv <mausilva828@gmail.com>
2019-11-19 11:55:53 -0300

    routes for authentication

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Text } from 'react-native';
 
 import './config/ReactotronConfig';
+import Routes from './routes';
 
 export default function App() {
-  return <Text>Hello world</Text>;
+  return <Routes />;
 }
