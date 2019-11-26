532121b7ed6cb5e517b62d8ec7387f68c96a1df5
mauriciosilv <mausilva828@gmail.com>
2019-11-26 14:35:50 -0300

    Input and Button components

--- mobile/src/components/Background/index.js
+++ mobile/src/components/Background/index.js
@@ -2,7 +2,7 @@ import LinearGradient from 'react-native-linear-gradient';
 import styled from 'styled-components/native';
 
 export default styled(LinearGradient).attrs({
-  colors: ['#124676', '#008080'],
+  colors: ['#124676', '#008099'],
 })`
   flex: 1;
 `;
