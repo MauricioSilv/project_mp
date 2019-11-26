561836548626bd22d228ca81549ceb9c0d401fd8
mauriciosilv <mausilva828@gmail.com>
2019-11-26 15:16:03 -0300

    SignIn page

--- mobile/src/pages/SignIn/index.js
+++ mobile/src/pages/SignIn/index.js
@@ -1,22 +1,42 @@
 import React from 'react';
-import { Text } from 'react-native';
 
-import Button from '~/components/Button';
-import Input from '~/components/Input';
 import Background from '~/components/Background';
-// import { Container } from './styles';
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+  TextCenter,
+} from './styles';
 
 export default function SignIn() {
   return (
     <Background>
-      <Text>SignIn</Text>
+      <Container>
+        <TextCenter>MP</TextCenter>
 
-      <Input
-        style={{ marginTop: 30 }}
-        icon="call"
-        placeholder="Digite seu nome"
-      />
-      <Button>Entrar</Button>
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+          />
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Sua senha secreta"
+          />
+          <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+        </Form>
+
+        <SignLink onPress={() => {}}>
+          <SignLinkText>Criar cadastro</SignLinkText>
+        </SignLink>
+      </Container>
     </Background>
   );
 }
